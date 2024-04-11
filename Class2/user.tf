aa09cb81075d9f5efa8f484e2ce39d6a3c347c27
AidaOmir <aidaomirbai@icloud.com>
2024-04-11 03:24:20 +0000

    added class3 files

--- Class2/user.tf
+++ Class2/user.tf
@@ -12,15 +12,15 @@ resource "aws_iam_group" "hello" {
   name = "devops"
 }
 
-# resource "aws_iam_group_membership" "team" {
-#   name = "tf-testing-group-membership"
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
 
-#   users = [
-#      for i in aws_iam_user.lb : i.name
-#   ]
+  users = [
+     for i in aws_iam_user.lb : i.name
+  ]
 
-#   group = aws_iam_group.hello.name
-# }
+  group = aws_iam_group.hello.name
+}
 
 
 
